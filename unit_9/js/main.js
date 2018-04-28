8efc3c3bcb4e6b447224206488098f610cd9346b
Taryn <taryn@sugarpush.com.au>
2018-04-28 11:46:11 +1000

    updates to everything

--- unit_9/js/main.js
+++ unit_9/js/main.js
@@ -1,4 +1,7 @@
-console.log("main connected");
+console.log("Hi there! Well, look at you checking out the console of my website! You must be one of those fancy developer type people!");
+console.log("Want to hire me? You should!");
+console.log("Hit me up via taryn@taryn.codes and we can have a chat!");
+console.log("I also really like to chat about dogs, so if you have a dog or even just a photo of a dog, then we can chat about that too!");
 
 $(document).ready( function() {
 $(window).scroll(function(){
